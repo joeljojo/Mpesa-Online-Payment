6394ad4e255384ef16d005710eacab5c81df3806
joeljojo <joelpamphyl@gmail.com>
2023-01-27 21:22:44 +0300

    Added code styling using eslint and prettier

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require("express");
+const express = require('express');
 const PORT = 5500 || process.env.PORT;
-
 const app = express();
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
